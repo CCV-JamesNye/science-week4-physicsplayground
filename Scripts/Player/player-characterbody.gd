a84c4f6c223e9e6504770634f243441f03044484
Eric Rivera <ear00053@vsc.edu>
2026-02-23 23:39:02 -0500

    Add Gravity
    Adds Gravity to the player, and removes free upwards and downwards movement.

--- Scripts/Player/player-characterbody.gd
+++ Scripts/Player/player-characterbody.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 # Determines how fast the Player will move
 var speed : float = 350
+@export var gravity : float = 980.0
 
 
 
@@ -17,16 +18,13 @@ func _process(delta: float) -> void:
 func _physics_process(delta: float) -> void:
 	# Stores current direction
 	var direction : Vector2 = Vector2.ZERO
+	velocity.y += gravity * delta
 	
 	# Reads input
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
 	if Input.is_action_pressed("move_left"):
 		direction.x -= 1
-	if Input.is_action_pressed("move_down"):
-		direction.y += 1
-	if Input.is_action_pressed("move_up"):
-		direction.y -= 1
 	
 	velocity = direction.normalized() * speed
 	move_and_slide()
