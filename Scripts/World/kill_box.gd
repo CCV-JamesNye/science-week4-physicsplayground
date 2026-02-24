fa5da237451933c387a6a224f61b065aa4a108f5
Eric Rivera <ear00053@vsc.edu>
2026-02-24 11:19:06 -0500

    Killbox Debug
    Fixed error where the Killbox wasn't recognizing the Player.

--- Scripts/World/kill_box.gd
+++ Scripts/World/kill_box.gd
@@ -6,5 +6,5 @@ func _ready() -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
-		print ('Player Died')
+		print("Player Died")
 	pass # Replace with function body.
