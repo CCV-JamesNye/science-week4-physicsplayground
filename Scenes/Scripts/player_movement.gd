84787b13d25644a70d85ca6e220b83305a148ec1
Eric Rivera <ear00053@vsc.edu>
2026-02-23 21:26:33 -0500

    Added Player Movement
    Added scripts for Player movement from previous week. Will likely use this repository for future projects.

---
+++ Scenes/Scripts/player_movement.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+# Determines how fast the Player will move
+var speed : float = 350
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	# Stores current direction
+	var direction : Vector2 = Vector2.ZERO
+	
+	# Reads input
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_down"):
+		direction.y += 1
+	if Input.is_action_pressed("move_up"):
+		direction.y -= 1
+	
+	position += direction.normalized() * speed * delta
+	pass
