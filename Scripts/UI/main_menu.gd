9c622cfa77aaae20084b9cd00f8932c54ac37419
Eric Rivera <ear00053@vsc.edu>
2026-03-07 12:27:29 -0500

    Finished Main Menu
    Finished the Main Menu and set it as the main Scene.

--- Scripts/UI/main_menu.gd
+++ Scripts/UI/main_menu.gd
@@ -1,11 +1,17 @@
 extends Control
 
+@onready var start_play: Button = $"MarginContainer/Panel/MarginContainer/VBoxContainer/Start-Play"
+@onready var quit_eject: Button = $"MarginContainer/Panel/MarginContainer/VBoxContainer/Quit-Eject"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	start_play.pressed.connect(_start_game)
+	quit_eject.pressed.connect(_quit_game)
 	pass # Replace with function body.
 
+func _quit_game() -> void:
+	get_tree().quit()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _start_game() -> void:
+	get_tree().change_scene_to_file("res://Scenes/week_4_work.tscn")
 	pass
